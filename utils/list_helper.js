6260cc3fb31598407f2e43e087c41e636f9aade2
Antti Onttonen <antti@neubit.fi>
2022-08-25 10:17:02 +0300

    part_4 exercise_4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,11 +2,14 @@ const dummy = () => {
   return 1;
 };
 
-const totalLikes = (blogs) => {
-  return blogs.reduce((previous, current) => previous + current.likes, 0);
-};
+const totalLikes = (blogs) =>
+  blogs.reduce((previous, current) => previous + current.likes, 0);
+
+const favoriteBlog = (blogs) =>
+  blogs.reduce((max, current) => (current.likes > max.likes ? current : max));
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
