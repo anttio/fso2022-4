7bc96bf9d1d9f560383d2db70651d07c754446ef
Antti Onttonen <antti@neubit.fi>
2022-08-30 14:09:26 +0300

    part_4 exercise_4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,30 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   res.json(blogs);
 });
 
 blogsRouter.post('/', async (req, res, next) => {
+  const { title, author, url, likes, userId } = req.body;
+  console.log('iiddee', userId);
+
+  const user = await User.findById(userId);
+
   try {
-    const blog = new Blog(req.body);
+    const blog = new Blog({
+      title,
+      author,
+      url,
+      likes,
+      user: user._id,
+    });
     const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
     res.json(savedBlog);
   } catch (e) {
     next(e);
